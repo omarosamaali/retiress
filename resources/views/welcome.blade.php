b489da7d3889907f08ae6f0e85954683e0d6ace6
unknown <987omar123osama456@gmail.com>
2025-08-14 10:52:10 +0300

    omar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -330,8 +330,8 @@ font-size: 16px;
         <div class="cloud cloud3" style="animation-delay: -50s;"></div>
     </div>
 
-<div id="headerholdert" style="position: relative; width: 100%; height: 82vh; overflow: hidden;">
-    <img src="{{ Storage::url($banner->image) }}" alt="Header Background" style="width: 100%; height: 100%; object-fit: cover;">
+<div id="headerholdert" style="position: relative; width: 100%; height: 82vh; overflow: hidden;     margin-top: 145px;">
+    <img src="{{ Storage::url($banner->image) }}" alt="Header Background" style="width: 100%; height: 100%; object-fit: fill;">
     <div class="swiper quoteSwiper">
         <div class="swiper-wrapper">
             @foreach (\App\Models\Slider::where('is_active', true)->get() as $slider)
